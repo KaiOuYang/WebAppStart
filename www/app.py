f33b58b30d62cf7fc8595340468be0d3069f9c63
yangk <710212101@qq.com>
2020-07-25 17:28:24 +0800

    start WebApp

---
+++ www/app.py
@@ -0,0 +1,23 @@
+
+import logging;logging.basicConfig(level=logging.INFO)
+
+import asyncio,os,json,time
+from datetime import datetime
+from aiohttp import web
+
+
+def index(request):
+    return web.Response(body=b"<h1>Awesome</h1>",content_type='text/html')
+
+
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET','/',index)
+    srv = yield from loop.create_server(app._make_handler(),'127.0.0.1',9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+if '__main__' == __name__:
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init(loop))
+    loop.run_forever()
